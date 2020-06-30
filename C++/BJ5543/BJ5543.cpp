8606754d628cb22139cc2ba7dd41f521d9a1ca85
hyeyoon <hyeyooncho08@gmail.com>
2020-06-30 21:19:41 +0900

    update

--- C++/BJ5543/BJ5543.cpp
+++ C++/BJ5543/BJ5543.cpp
@@ -3,37 +3,24 @@
 using namespace std;
 
 int main(){
-    int burger[2];
-    int juice[1];
-    int b, j;
-    int sb, sj, total;
-    sb = burger[0];
-    sj = juice[0];
+    int burger[3];
+    int juice[2];
+    int sb=9999;
+    int sj=9999;
+    int total;
 
     for(int i=0; i<3; i++){
-        cin >> b;
-        while(b >=100 && b<=2000){
-            b = burger[i];
-        }
-    }
-    for(int i=0; i<2; i++){
-        cin >> j;
-        while(j >=100 && j<=2000){
-            j = juice[i];
-        }
-    }
-    for(int i=0; i<3; i++){
-        if(sb > burger[i]){
+        cin >> burger[i];
+        if(burger[i] < sb){
             sb = burger[i];
         }
     }
     for(int i=0; i<2; i++){
-        if(sj > juice[i]){
+        cin >> juice[i];
+        if(juice[i] < sj){
             sj = juice[i];
         }
     }
-    cout << sb;
-    cout << sj;
-    total = sb + sj - 500;
+    total = sb + sj - 50;
     cout << total;
 }
