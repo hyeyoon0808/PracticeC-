615e493e5cf9a8709da2ad9af00ac6dcf5c11fcc
hyeyoon <hyeyooncho08@gmail.com>
2020-07-02 23:46:23 +0900

    Update

--- C++/BJ2562/BJ2562.cpp
+++ C++/BJ2562/BJ2562.cpp
@@ -4,8 +4,14 @@ using namespace std;
 
 int main(){
     int x[9]; 
+    int biggest, order;
     for(int i=0; i < 9; i++){
         cin >> x[i];
+        if(x[i] < biggest){
+            biggest = x[i];
+            order = i;
+        }
     }
-    
+    cout << biggest;
+    cout << order;
 }
