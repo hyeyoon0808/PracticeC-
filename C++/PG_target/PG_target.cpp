79216c69b3b0bd8f79cb34ba45cb915339313307
hyeyoon <hyeyooncho08@gmail.com>
2020-07-22 16:42:10 +0900

    update

--- C++/PG_target/PG_target.cpp
+++ C++/PG_target/PG_target.cpp
@@ -2,9 +2,22 @@
 #include <vector>
 
 using namespace std;
+int answer =0;
+int numSize;
 
 int solution(vector<int> numbers, int target) {
-    int answer = 0;
-    
+    numSize = numbers.size();
+    dfs(0,0,target,numbers);    
     return answer;
+}
+
+void dfs(int idx, int sum, int target, vector<int> numbers){
+    if(idx == numSize){
+        if(sum == target){
+            answer++;
+        }
+        return;
+    }
+    dfs(idx + 1, sum+numbers[idx], target, numbers);
+    dfs(idx + 1, sum - numbers[idx], target, numbers);
 }
